ff3eaa8b7e96a66788c610b867f656064615ad2e
Steven Chen <chensteven945@gmail.com>
2023-03-15 00:28:27 -0400

    Cleaned data Created plot Normalized data to median Created Heatmap

--- hw2.py
+++ hw2.py
@@ -2,9 +2,27 @@ import numpy as np
 import pandas as pd
 import geopandas as gp
 from shapely import Point
+import matplotlib.pyplot as plt
 
+map = gp.read_file('md_cong_adopted_2022.zip')
 
+with open('weball22.txt') as file:
+    data = pd.read_csv(file, sep = '|', header = None)
 
-print('hello world')
-print('hello world')
-print('omgggggg tina~~~~~~')
+data = data.loc[data[18] == 'MD']
+data = data.loc[data[19] > 0]
+data = data.loc[:, [11, 17, 19, 25, 26]]
+data1 = data.groupby(by = 19, as_index = False).sum()
+data_sum = data1.sum(axis = 1)
+median = data_sum.median()
+
+for i in range(len(data_sum)):
+    data_sum[i] = (data_sum[i] - median) / median
+
+print(data_sum)
+map['con_diff'] = data_sum
+
+print('The amount has been normalized to the median of contributions across all districts')
+map.plot(column = 'con_diff', cmap = 'Greens', legend = True)
+plt.title('Contributions for each District in MD')
+plt.show()
