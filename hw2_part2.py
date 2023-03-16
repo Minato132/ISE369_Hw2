fc9d38d392a4ce1e53bcd08e0d0fd1213461439d
Steven Chen <chensteven945@gmail.com>
2023-03-15 23:25:40 -0400

    Finish up part 2

--- hw2_part2.py
+++ hw2_part2.py
@@ -3,3 +3,31 @@ import geopandas as gp
 import matplotlib.pyplot as plt
 
 map = gp.read_file('md_cong_adopted_2022.zip')
+
+with open('md_2022_primary_cand_data.csv') as file:
+    data = pd.read_csv(file)
+
+data = data.loc[:, ['Party', 'District', 'Primary %']]
+x = []
+for i in data['Primary %']:
+    i = i.replace('%', '')
+    x.append(float(i)/ 100)
+
+data['Primary Dec'] = x
+data.dropna(inplace = True)
+
+# The Dems
+
+dems = data.loc[data['Party'] == 'DEM']
+dems = dems.groupby(by = 'District', as_index = False).max()
+
+
+# The Rep
+
+reps = data.loc[data['Party'] == 'REP']
+reps = reps.groupby(by = 'District', as_index = False).max()
+
+diff = dems['Primary Dec'] - reps['Primary Dec']
+
+map.plot(column = diff, cmap =)
+print(diff)
